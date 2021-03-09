5b4f89945583af35dda9377381461e5be96c1791
Juan David Rodas Barco <zador-054@hotmail.com>
2021-03-08 19:33:06 -0500

    jornada terminada

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 '''Esta aplicacion mostrara las propiedades de la persona'''
 
 class Persona:
-    def __init__(self):
-        pass
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
         
     
     def saludo(self):
         print('este es un saludo')
         
 
-juan = Persona()
-juan.saludo()
+persona = Persona("juan", 26)
+persona.saludo()
