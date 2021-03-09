5576168f9dab81c780dc080c03fbab65c45f6cd5
Juan David Rodas Barco <jdrodas76720@umanizales.edu.co>
2021-03-09 10:58:47 -0500

    Se le agrego un metodo para calcular la edad de Ãla persona

--- app.py
+++ app.py
@@ -6,10 +6,17 @@ class Persona:
         self.edad = edad
         
     
-    def saludo(self):
+    def presentacion(self):
         '''Principal saludo'''
         print(f'Hola {self.nombre} espero que estes de buen humor hoy :)')
+
+    
+    @staticmethod
+    def fecha_de_nacimiento(fecha_actual):
+        birthday = self.edad - fecha_actual
+        print(f'La fecha de nacimiento de {self.nombre} es: {birthday}')
         
 
 persona = Persona("juan", 26)
 persona.saludo()
+persona.fecha_de_nacimiento(2021)
