ddd058b117b42ec60b3eac18cbe8b7da0291dd29
Juan David Rodas Barco <zador-054@hotmail.com>
2021-03-08 19:31:49 -0500

    Se agrego un constructor y se le cambio el nombre al objeto persona

---
+++ app.py
@@ -0,0 +1,13 @@
+'''Esta aplicacion mostrara las propiedades de la persona'''
+
+class Persona:
+    def __init__(self):
+        pass
+        
+    
+    def saludo(self):
+        print('este es un saludo')
+        
+
+juan = Persona()
+juan.saludo()
